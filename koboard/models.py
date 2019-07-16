681209c5fc7163d167ff793c99219a6aea171c9c
Byeonggon Lee <gonny952@gmail.com>
2019-07-16 16:14:34 +0900

    Modify koboard/models.py

--- koboard/models.py
+++ koboard/models.py
@@ -1,6 +1,11 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+class Board(models.Model):
+    name = models.TextField(null=False, blank=False)
+
 class Article(models.Model):
-    title = models.TextField()
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, blank=False, null=False, on_delete=models.CASCADE)
+    board = models.ForeignKey(Board, blank=False, null=False, on_delete=models.CASCADE)
+    title = models.TextField(null=False, blank=False)
     content = models.TextField(null=True, blank=True)
