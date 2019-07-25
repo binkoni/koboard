d2ab32467e11ea20501a350313cd76d4e7904e2c
Byeonggon Lee <gonny952@gmail.com>
2019-07-25 21:23:05 +0900

    Update models

--- koboard/models.py
+++ koboard/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.conf import settings
+import django.utils.timezone
 
 class Board(models.Model):
     name = models.CharField(max_length=64, null=False, blank=False)
@@ -8,6 +9,9 @@ class Article(models.Model):
     prev = models.ForeignKey('self', blank=True, null=True, on_delete=models.CASCADE)
     user = models.ForeignKey(settings.AUTH_USER_MODEL, blank=False, null=False, on_delete=models.CASCADE)
     board = models.ForeignKey(Board, blank=False, null=False, on_delete=models.CASCADE)
+    ctime = models.DateTimeField(blank=False, null=False, default=timezone.now, editable=False)
+    mtime = models.DateTimeField(blank=False, null=False, default=timezone.now, editable=False)
+    views = models.IntegerField(blank=False, null=False, default=0, editable=False)
     title = models.CharField(max_length=64, blank=False, null=False)
     content = models.TextField(blank=True, null=False)
 
@@ -15,6 +19,8 @@ class Comment(models.Model):
     prev = models.ForeignKey('self', blank=True, null=True, on_delete=models.CASCADE)
     user = models.ForeignKey(settings.AUTH_USER_MODEL, blank=False, null=False, on_delete=models.CASCADE)
     article = models.ForeignKey(Article, blank=False, null=False, on_delete=models.CASCADE)
+    ctime = models.DateTimeField(blank=False, null=False, default=timezone.now, editable=False)
+    mtime = models.DateTimeField(blank=False, null=False, default=timezone.now, editable=False)
     content = models.TextField(blank=True, null=False)
 
 class File(models.Model):
