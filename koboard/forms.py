466d982f86118ae078c6ea552235dc11447357d3
Byeonggon Lee <gonny952@gmail.com>
2019-07-25 20:10:36 +0900

    Upate forms and models

--- koboard/forms.py
+++ koboard/forms.py
@@ -4,6 +4,22 @@ from .models import *
 class ArticleModelForm(forms.ModelForm):
     class Meta:
         model = Article
+        exclude = ('user',)
+        widgets = {'prev': forms.HiddenInput()}
+
 class CommentModelForm(forms.ModelForm):
     class Meta:
         model = Comment
+        exclude = ('user',)
+        widgets = {
+            'prev': forms.HiddenInput(),
+            'article': forms.HiddenInput()
+        }
+
+class FileModelForm(forms.ModelForm):
+    class Meta:
+        model = File
+
+class ImageModelForm(forms.ModelForm):
+    class Meta:
+        model = Image
--- koboard/models.py
+++ koboard/models.py
@@ -11,6 +11,12 @@ class Article(models.Model):
     title = models.CharField(max_length=64, blank=False, null=False)
     content = models.TextField(blank=True, null=False)
 
+class Comment(models.Model):
+    prev = models.ForeignKey('self', blank=False, null=True, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, blank=False, null=False, on_delete=models.CASCADE)
+    article = models.ForeignKey(Article, blank=False, null=False, on_delete=models.CASCADE)
+    content = models.TextField(blank=True, null=False)
+
 class File(models.Model):
     article = models.ForeignKey(Article, blank=False, null=False, on_delete=models.CASCADE)
     file = models.FileField()
@@ -18,11 +24,11 @@ class File(models.Model):
 class Image(models.Model):
     image = models.ImageField()
 
-class Comment(models.Model):
-    prev = models.ForeignKey('self', blank=False, null=True, on_delete=models.CASCADE)
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, blank=False, null=False, on_delete=models.CASCADE)
+class FileArticleXref(models.Model):
+    file = models.ForeignKey(File, blank=False, null=False, on_delete=models.CASCADE)
     article = models.ForeignKey(Article, blank=False, null=False, on_delete=models.CASCADE)
-    content = models.TextField(blank=True, null=False)
+    class Meta:
+        unique_together = (('file', 'article'),)
 
 class ImageArticleXref(models.Model):
     image = models.ForeignKey(Image, blank=False, null=False, on_delete=models.CASCADE)
